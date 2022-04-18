22c48c038ca4723549fb7c364ae8b2437dbe412f
Komal <divyavairagade@gmail.com>
2022-04-19 00:02:12 +0530

    prime number added

---
+++ Assign06_PP/Class3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign06_PP
+{
+    internal class Class3
+    {
+        public static void PrimeNumber()
+        {
+            int i, n, b = 0;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 2; i <= n/2; i++)
+            {
+                int result = n % i;
+                if (result == 0)
+                {
+                    b++;
+                    break;
+                }
+                
+            }
+            
+            if (b == 0 && n != 1)
+            {
+                Console.WriteLine(n + " Given number is prime");
+            }
+            else
+            {
+                Console.WriteLine(n + " Given number is not prime");
+            }
+
+        }
+    }
+}
--- Assign06_PP/Program.cs
+++ Assign06_PP/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Day 6 Assignment");
 //Assign06_PP.Class1.Fibonacci_Iterative(9);
-Assign06_PP.Class2.Perfectnumber();
+//Assign06_PP.Class2.Perfectnumber();
+Assign06_PP.Class3.PrimeNumber();
