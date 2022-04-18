3217c28c1c371142b92157c5d4c2a425ba5a31f0
Komal <divyavairagade@gmail.com>
2022-04-19 00:17:36 +0530

    reverse the number program added

--- Assign06_PP/Program.cs
+++ Assign06_PP/Program.cs
@@ -2,4 +2,5 @@
 Console.WriteLine("Day 6 Assignment");
 //Assign06_PP.Class1.Fibonacci_Iterative(9);
 //Assign06_PP.Class2.Perfectnumber();
-Assign06_PP.Class3.PrimeNumber();
+//Assign06_PP.Class3.PrimeNumber();
+Assign06_PP.Class4.ReverseNumber();
---
+++ Assign06_PP/Stopwatch.cs
@@ -0,0 +1,14 @@
+﻿namespace Assign06_PP
+{
+    internal class Stopwatch
+    {
+        public Stopwatch()
+        {
+        }
+
+        internal static object StartNew()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
