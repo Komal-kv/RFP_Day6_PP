dc32d938cd987b40b83ecb44ed75b510bf4b5351
Komal <divyavairagade@gmail.com>
2022-04-18 23:28:13 +0530

    fibonacci series

--- Assign06_PP/Class1.cs
+++ Assign06_PP/Class1.cs
@@ -8,6 +8,19 @@ namespace Assign06_PP
 {
     internal class Class1
     {
+        public static void Fibonacci_Iterative(int n)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine("{0} {1} ",a, b);
 
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.WriteLine("{0}", c);
+
+                a = b;
+                b = c;
+            }
+        }
     }
 }
--- Assign06_PP/Program.cs
+++ Assign06_PP/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Day 6 Assignment");
+Assign06_PP.Class1.Fibonacci_Iterative(9);
