385b7c0626d6288b430a51f8424d6e07c83d63cf
Komal <divyavairagade@gmail.com>
2022-04-18 23:39:09 +0530

    perfect number program added

--- Assign06_PP/Program.cs
+++ Assign06_PP/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Day 6 Assignment");
-Assign06_PP.Class1.Fibonacci_Iterative(9);
+//Assign06_PP.Class1.Fibonacci_Iterative(9);
+Assign06_PP.Class2.Perfectnumber();
